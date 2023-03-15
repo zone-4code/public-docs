104adc79491be73096077a6e45aacb66d6a3ffd3
yassinebensalem <49484211+yassinebensalem@users.noreply.github.com>
2023-03-15 12:44:06 +0100

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,21 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/docs-public/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
+
+  
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zone-4code', // Usually your GitHub org/user name.
+  projectName: 'public-docs', // U
+
+
+ 
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
